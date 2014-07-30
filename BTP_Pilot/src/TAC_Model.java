5e1e533c0b49f6fe7c7ce680f6e7e709d63ed76d
vivekaditya <vivekadityapurella@gmail.com>
2014-07-30 15:38:44 +0530

    Core NLP added

--- BTP_Pilot/src/TAC_Model.java
+++ BTP_Pilot/src/TAC_Model.java
@@ -2,29 +2,43 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List; 
+import java.util.List;  
 
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 public class TAC_Model {
 
 	/**
 	 * @param args
-	 * @throws IOException 
-	 * http://www.berouge.com/Pages/DownloadROUGE.aspx
+	 * @throws IOException  
+	 * @throws ClassNotFoundException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		
 		System.out.println("Start");
 		BufferedReader br = new BufferedReader(new FileReader("TAC_2014_BiomedSumm_Training_Data/data/D1401_TRAIN/Annotation/D1401_TRAIN.A.ann.txt"));
 		String[] Annotator_line ;
 		String line = br.readLine();
+		MaxentTagger tagger = new MaxentTagger("models/left3words-wsj-0-18.tagger");
+//	 
+//	        // The sample string
+//	        String sample = "This is a sample text";
+//	 
+//	        // The tagged string
+//	        String tagged = tagger.tagString(sample);
+//	 
+//	        // Output the result
+//	        System.out.println(tagged);
 		while(line!=null)
 		{
 			Annotator_line = line.split("\\|");
 //			System.out.println(Annotator_line.length);
 	 		int i;
 	 		System.out.println();
-	 		System.out.println(Annotator_line[7].split(":")[1]);
+//	 		System.out.println(Annotator_line[7].split(":")[1]);
+//	 		System.out.println(Annotator_line[9].split(":")[1]);
+	 		String tagged = tagger.tagString(Annotator_line[7].split(":")[1]);	 
+	        System.out.println(tagged);
 //	 		for (i=0;i<Annotator_line.length;i++)
 //	 			{
 //	 				System.out.println((Annotator_line[i].split(":")[0])+"\t"+ Annotator_line[i].split(":")[1]);			
@@ -32,13 +46,6 @@ public class TAC_Model {
 	 		String empty = br.readLine();
 	 		line = br.readLine();
 		}
-		System.out.println("End");
-//		String serializedClassifier = "classifiers/ner-eng-ie.crf-3-all2008.ser.gz";
-//		AbstractSequenceClassifier classifier = CRFClassifier.getClassifierNoExceptions(serializedClassifier);
-//	    String s1 = "Good afternoon Rajat Raina, how are you today?";
-//	    String s2 = "I go to school at Stanford University, which is located in California.";
-//	    System.out.println(classifier.classifyToString(s1));
-//	    System.out.println(classifier.classifyWithInlineXML(s2));
-//	    System.out.println(classifier.classifyToString(s2, "xml", true));
+		System.out.println("End"); 
 	} 
 }  
